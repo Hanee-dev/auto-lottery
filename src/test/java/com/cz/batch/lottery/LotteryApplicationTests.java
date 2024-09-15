d82a1edff77e82c481dc01531c51305b139a04a5
Hanee-dev <hanee.devel@gmail.com>
2024-09-15 16:31:26 +0900

    1. boot 기본 테스트 주석처리

--- src/test/java/com/cz/batch/lottery/LotteryApplicationTests.java
+++ src/test/java/com/cz/batch/lottery/LotteryApplicationTests.java
@@ -3,7 +3,7 @@ package com.cz.batch.lottery;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class LotteryApplicationTests {
 
 	@Test
